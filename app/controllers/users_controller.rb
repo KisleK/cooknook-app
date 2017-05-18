04192519c9085e9b19994fc9faf09dc167f57078
KisleK <kisle.kuhn1@gmail.com>
2017-05-17 19:28:09 -0500

    sessions controller(login/out)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ def new
 end
 
 def create
-  user = User.new(name: parmas[:name],
+  user = User.new(name: params[:name],
                   email: params[:email],
                   password: params[:password],
                   password_confirmation: params[:password_confirmation]
@@ -13,14 +13,12 @@ def create
   if user.save
     session[:user_id] = user.id
     flash[:success] = 'Successfully created account'
-    reditect_to '/'
+    redirect_to '/'
 
   else
     flash[:warning] = 'Invalid email or password'
-    redirect_to '/signup'
-  end
-
+    redirect_to '/users'
+  end  
 end
-
 
 end
